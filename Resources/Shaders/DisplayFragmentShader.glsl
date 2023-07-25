bd817671b37c9fb64718e61e25b764126e685242
Nikita-Dudorov <zoom_ccss@mail.ru>
2023-07-25 19:31:27 -0400

    typo

--- Resources/Shaders/DisplayFragmentShader.glsl
+++ Resources/Shaders/DisplayFragmentShader.glsl
@@ -3,7 +3,7 @@
 uniform sampler2D imageTex;
 uniform int width;
 uniform int height;
-out vec4 colorResponse; // Shader output: the color response attached to this fragment. here the simlple content of the bounded image texture
+out vec4 colorResponse; // Shader output: the color response attached to this fragment. here the simple content of the bounded image texture
 
 void main () {
 	colorResponse = vec4 (texture(imageTex, vec2 (gl_FragCoord.x/width, gl_FragCoord.y/height)).rgb, 1.0);	
