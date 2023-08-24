350f0c295eda9e72b6f3d2c3c59d3f2a1bc5779a
Nikita-Dudorov <zoom_ccss@mail.ru>
2023-08-24 18:27:35 +0300

    transparency shaders

---
+++ Resources/Shaders/TransparencyVertexShader.glsl
@@ -0,0 +1,25 @@
+#version 410 core // Minimal GL version support expected from the GPU
+
+layout(location=0) in vec3 vPosition; // The 1st input attribute is the position (CPU side: glVertexAttrib 0)
+layout(location=1) in vec3 vNormal;
+layout(location=2) in vec2 vTextCoord;
+
+uniform mat4 modelMat, viewMat, projectionMat, normalMat;
+
+out vec3 fPosition;
+out vec3 fNormal;
+out vec2 fTextCoord;
+
+void main() {
+    vec4 p = viewMat * modelMat * vec4 (vPosition, 1.0);
+    if (vNormal == vec3(0.0)) { // screen texture
+        gl_Position =  vec4 (vPosition, 1.0);
+    }
+    else {
+        gl_Position =  projectionMat * p; // mandatory to fire rasterization properly
+    }
+    vec4 n = normalMat * vec4 (vNormal, 1.0);
+    fPosition = p.xyz;
+    fNormal = normalize (n.xyz);
+    fTextCoord = vTextCoord;
+}
